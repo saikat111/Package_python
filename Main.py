acdd6d8642718ac024c1e4c490e1fd88e06afe47
al rakib sorder <46457169+saikat111@users.noreply.github.com>
2019-10-19 00:01:00 +0600

    Create Main.py
    Main code

---
+++ Main.py
@@ -0,0 +1,16 @@
+# Import classes from your brand new package 
+from Cars import Bmw 
+from Cars import Audi 
+from Cars import Nissan 
+
+# Create an object of Bmw class & call its method 
+ModBMW = Bmw() 
+ModBMW.outModels() 
+
+# Create an object of Audi class & call its method 
+ModAudi = Audi() 
+ModAudi.outModels() 
+
+# Create an object of Nissan class & call its method 
+ModNissan = Nissan() 
+ModNissan.outModels() 
